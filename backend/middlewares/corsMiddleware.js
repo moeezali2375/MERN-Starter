7d316bae01aa4e70739ecb8f6578a39b61c08afc
moeezali <moeezali2375@gmail.com>
2024-07-12 20:29:38 +0500

    Add CORS middleware

---
+++ backend/middlewares/corsMiddleware.js
@@ -0,0 +1,20 @@
+const cors = require("cors");
+
+const crossOrigin = (app) => {
+  if (process.env.NODE_ENV === "dev") {
+    //! Development environment
+    app.use(
+      cors({
+        origin: process.env.DEV_URL,
+      })
+    );
+  } else {
+    //! Production environment
+    app.use(
+      cors({
+        origin: process.env.PROD_URL,
+      })
+    );
+  }
+};
+module.exports = { crossOrigin };
