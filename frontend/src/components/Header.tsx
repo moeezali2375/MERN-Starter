fc5039056525e46543128a560c7d2306eeb72dbb
moeezali <moeezali2375@gmail.com>
2024-08-01 13:59:47 +0500

    Header Fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,7 +14,7 @@ import { ModeToggle } from "./mode-toggle";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="top-0  flex h-16 items-center gap-4 border-b px-4 md:px-6  ">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           to="/"
