2593c052805218664e6c70faf60b10855d21b8ef
moeezali <moeezali2375@gmail.com>
2024-08-08 23:21:10 +0500

    BUG

---
+++ frontend/src/components/ProtectedComp.tsx
@@ -0,0 +1,33 @@
+import { useAxios } from "@/context/AxiosProvider";
+import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
+
+const ProtectedComp = () => {
+  const axiosInstance = useAxios();
+  const { toast } = useToast();
+
+  const checkStatus = async () => {
+    try {
+      await axiosInstance.get("/protect");
+      toast({
+        title: "All Okay.",
+        description: "You are Authorized.",
+      });
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        description: "Token expired",
+      });
+    }
+  };
+  return (
+    <div className="text-center allign-center">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center py-10">
+        You are logged in!
+      </h1>
+      <Button onClick={checkStatus}>Check Status</Button>
+    </div>
+  );
+};
+
+export default ProtectedComp;
