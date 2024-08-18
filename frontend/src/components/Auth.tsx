0cb9fe397512fbddb6cc7cb28290683072de7600
moeezali <moeezali2375@gmail.com>
2024-08-18 16:49:52 +0500

    Css Fix

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -4,9 +4,9 @@ import Signup from "@/components/Signup";
 
 export default function Auth() {
   return (
-    <div className="flex items-center justify-center min-h-screen ">
-      <Tabs defaultValue="signup" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
+    <div className="flex items-center justify-center mt-12">
+      <Tabs defaultValue="signup" className="w-[400px] mr-2 ml-2">
+        <TabsList className="grid w-full grid-cols-2 mb-4">
           <TabsTrigger value="login">Log In</TabsTrigger>
           <TabsTrigger value="signup">Sign Up</TabsTrigger>
         </TabsList>
