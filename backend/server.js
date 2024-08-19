a3d48fc1a5e0e30ba6748b683dbb7b09c0fc4f8e
moeezali <moeezali2375@gmail.com>
2024-08-19 22:32:55 +0500

    Deploy Phase

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const homeRoutes = require("./routes/routes");
 const authRoutes = require("./routes/authRoutes");
 const protectRoutes = require("./routes/protectRoutes");
 const { crossOrigin } = require("./middlewares/corsMiddleware");
+const path = require("path");
 
 const app = express();
 
@@ -17,7 +18,20 @@ app.use("/api", homeRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/protect", protectRoutes);
 
-app.get("*", (req, res) => res.redirect("/api"));
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(path.dirname(__dirname1), "frontend", "dist"))
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(path.dirname(__dirname1), "frontend", "dist", "index.html")
+    );
+  });
+} else {
+  app.get("*", (req, res) => res.redirect("/api"));
+}
 
 const PORT = process.env.PORT;
 
